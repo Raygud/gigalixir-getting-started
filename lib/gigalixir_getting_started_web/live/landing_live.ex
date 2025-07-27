81414fe2c114af9f3f8c82edfd89bcf37653ebb0
Rúni Gudmundarson <78358597+Raygud@users.noreply.github.com>
2025-07-28 01:47:41 +0200

    Deploy static songs

--- lib/gigalixir_getting_started_web/live/landing_live.ex
+++ lib/gigalixir_getting_started_web/live/landing_live.ex
@@ -6,20 +6,18 @@ defmodule GigalixirGettingStartedWeb.LandingLive do
     <div class="bg-gray-900 text-gray-100 min-h-screen flex flex-col items-center">
       <!-- Image pinned at top -->
       <img src="/images/landing.png" alt="Ólavsøka fólk" class="w-full max-w-md shadow-lg" />
-      <h1
-        id="welcome-title"
-        phx-hook="StopBounce"
-        class="text-4xl md:text-6xl font-extrabold text-pink-400 drop-shadow-md animate-bounce ml-5"
-        style="transform: rotate(351deg)"
-      >
-        Vælkomin til Ólavsøku 2025!
-      </h1>
-      <!-- Centered content block -->
-      <div class="flex-1 flex flex-col items-center justify-center text-center space-y-6 max-w-3xl">
-        <h2 class="text-xl md:text-2xl text-yellow-400 italic">
+      <div id="welcome-title" phx-hook="StopBounce" class="ml-5" style="transform: rotate(351deg)">
+        <h1 class="text-4xl md:text-6xl font-extrabold text-pink-400 drop-shadow-md inline">
+          Vælkomin til Ólavsøku 2025!
+        </h1>
+        
+        <h2 class="text-xl md:text-2xl text-yellow-400 italic inline">
           Í "Gula Húsinum"
         </h2>
-        
+      </div>
+      
+    <!-- Centered content block -->
+      <div class="flex-1 flex flex-col items-center justify-center text-center space-y-6 max-w-3xl">
         <nav class="mt-10 grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4">
           <.link
             navigate="/songs"
